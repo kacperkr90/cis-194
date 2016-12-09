9f5ff9b62cbb1b4990a80c9fbc165b3c65bd1d8d
kacper <kacper.krycki@gmail.com>
2016-12-09 21:09:03 +0100

    Function 'parse' completed

--- lesson02/LogAnalysis.hs
+++ lesson02/LogAnalysis.hs
@@ -4,38 +4,41 @@ module LogAnalysis where
 
 import Log
 
--- parseMessage :: String -> LogMessage
--- parseMessage message = (words message)
+parse :: String -> [LogMessage]
+parse content = map parseMessage (lines content) 
+
+parseMessage :: String -> LogMessage
+parseMessage = createLogMessage . words 
+
+type Parameters = [String]
+
+createLogMessage :: Parameters -> LogMessage
+createLogMessage [] = emptyUnknownMessage 
+createLogMessage params@(x : _)
+  | isAnyKnownType x = createIdentifiedLogMessage (getMessageType params) params
+  | otherwise        = Unknown (unwords params)
+  where
+      isAnyKnownType t = t `elem` ["I", "W", "E"]
 
 emptyUnknownMessage :: LogMessage
 emptyUnknownMessage = Unknown ""
 
-type Parameters = [String]
-
-metoda :: Parameters -> LogMessage
-metoda [] = emptyUnknownMessage 
-metoda params@(x : _)
-	| isAnyKnownType x  = LogMessage (getMessageType (take 2 params)) (getTimestamp params) (getMessageContent params)  
-	| otherwise 		= Unknown (unwords params) 
-	where
-		isAnyKnownType t = t `elem` ["I", "W", "E"]	 
-
 getMessageType :: Parameters -> MessageType
-getMessageType params = case (take 2 params) of
-	["E", y] -> Error (read y)
-	["I", _] -> Info
-	_ -> Warning
+getMessageType params = case take 2 params of
+  ["E", y] -> Error (read y)
+  ["I", _] -> Info
+  _        -> Warning
+
+createIdentifiedLogMessage :: MessageType -> Parameters -> LogMessage
+createIdentifiedLogMessage (Error s) params = LogMessage (Error s) (getTimestamp (drop2 params)) (getMessageContent (drop2 params))
+createIdentifiedLogMessage t (_:xs)         = LogMessage t (getTimestamp xs) (getMessageContent xs)
+createIdentifiedLogMessage _ _              = emptyUnknownMessage
 
 getTimestamp :: Parameters -> Int
-getTimestamp = read . head . (drop 2)
+getTimestamp = read . head
+
+drop2 :: [String] -> [String]
+drop2 = drop 2
 
 getMessageContent :: Parameters -> String
-getMessageContent = unwords . (drop 3)
-
-
--- parseLogType :: [String] -> LogMessage
--- parseLogType (x:y:xs)
--- 	| isJust (read y) -> 
-
-	-- LogMessage t 1 "test"
-	-- Unknown ""
+getMessageContent = unwords . drop 1
