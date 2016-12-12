0d2a5933c3c600c1d52bdb0c2b012706b668f3fe
kacper <kacper.krycki@gmail.com>
2016-12-12 01:00:22 +0100

    inOrder function in progress

--- lesson02/LogAnalysis.hs
+++ lesson02/LogAnalysis.hs
@@ -34,7 +34,7 @@ createIdentifiedLogMessage (Error s) params = LogMessage (Error s) (getTimestamp
 createIdentifiedLogMessage t (_:xs)         = LogMessage t (getTimestamp xs) (getMessageContent xs)
 createIdentifiedLogMessage _ _              = emptyUnknownMessage
 
-getTimestamp :: Parameters -> Int
+getTimestamp :: Parameters -> TimeStamp 
 getTimestamp = read . head
 
 drop2 :: [String] -> [String]
@@ -42,3 +42,32 @@ drop2 = drop 2
 
 getMessageContent :: Parameters -> String
 getMessageContent = unwords . drop 1
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert m Leaf = createEmptyNode m
+insert m (Node _ (Unknown _) _) = createEmptyNode m
+insert m@(LogMessage _ ct _) (Node l m2@(LogMessage _ pt _) r) 
+  | ct < pt = case l of
+    Leaf -> Node (createEmptyNode m) m2 r
+    lmt  -> Node (insert m lmt) m2 r
+  | otherwise = case l of
+    Leaf -> Node l m2 (createEmptyNode m)
+    rmt  -> Node l m2 (insert m rmt)
+
+
+createEmptyNode :: LogMessage -> MessageTree
+createEmptyNode lm = Node Leaf lm Leaf
+
+build :: [LogMessage] -> MessageTree
+build messages = buildTree messages Leaf 
+
+buildTree :: [LogMessage] -> MessageTree -> MessageTree
+buildTree [] t = t
+buildTree [x] t = insert x t
+buildTree (x:xs) t = buildTree xs (insert x t)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder (Node Leaf lm Leaf) = [lm]
+inOrder (Node lt lm gt) = inOrder lt ++ [lm] ++ inOrder gt
+inOrder Leaf = []
