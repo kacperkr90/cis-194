c3976ca90df0382643dd68ac2d1af8929c9d1439
kacper <kacper.krycki@gmail.com>
2017-03-26 01:39:26 +0100

    Lesson 7 intro #2

--- lesson07/Lesson07.hs
+++ lesson07/Lesson07.hs
@@ -40,3 +40,19 @@ flatten' = treeFold [] (\l x r -> l ++ [x] ++ r)
 
 treeMax :: (Ord a, Bounded a) => Tree a -> a
 treeMax = treeFold minBound (\l x r -> l `max` x `max` r)
+
+data ExprT = Lit Integer
+           | Add ExprT ExprT
+           | Mul ExprT ExprT
+
+eval :: ExprT -> Integer
+eval (Lit x)     = x
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Mul e1 e2) = eval e1 * eval e2
+
+exprTFold :: (Integer -> b) -> (b -> b -> b) (b -> b -> b) -> ExprT -> b
+exprTFold f _ _ (Lit i) = f i
+exprTFold f g h (Add e1 e2) = g (exprTFold f g h e1) (exprTFold f g h e2)
+exprTFold f g h (Mul e1 e2) = h (exprTFold f g h e1) (exprTFold f g h e2)
+
+
