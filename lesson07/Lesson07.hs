a867b22f8ba6218d5a01e6e966e72cf390ebe511
kacper <kacper.krycki@bms.com.pl>
2017-03-27 16:26:59 +0200

    Lesson 7 intro done

--- lesson07/Lesson07.hs
+++ lesson07/Lesson07.hs
@@ -50,9 +50,53 @@ eval (Lit x)     = x
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
 
-exprTFold :: (Integer -> b) -> (b -> b -> b) (b -> b -> b) -> ExprT -> b
+exprTFold :: (Integer -> b) -> (b -> b -> b) -> (b -> b -> b) -> ExprT -> b
 exprTFold f _ _ (Lit i) = f i
 exprTFold f g h (Add e1 e2) = g (exprTFold f g h e1) (exprTFold f g h e2)
 exprTFold f g h (Mul e1 e2) = h (exprTFold f g h e1) (exprTFold f g h e2)
 
+eval2 :: ExprT -> Integer
+eval2 = exprTFold (id) (+) (*)
 
+numLiterals :: ExprT -> Int
+numLiterals = exprTFold (const 1) (+) (+)
+
+-- class Monoid m where
+--   mempty :: m
+--   mappend :: m -> m -> m
+--   mconcat :: [m] -> m
+--   mconcat = foldr mappend mempty
+
+instance Monoid [a] where
+  mempty = []
+  mappend = (++)
+
+newtype Sum a = Sum a
+  deriving (Eq, Ord, Num, Show)
+
+getSum :: Sum a -> a
+getSum (Sum a) = a
+
+instance Num a => Monoid (Sum a) where
+  mempty = Sum 0
+  mappend = (+)
+
+newtype Product a = Product a
+  deriving (Eq, Ord, Num, Show)
+
+getProduct :: Product a -> a
+getProduct (Product a) = a
+
+instance Num a => Monoid (Product a) where
+  mempty = Product 1
+  mappend = (*)
+
+lst :: [Integer]
+lst = [1,5,8,23,423,99]
+
+prod :: Integer
+prod = getProduct . mconcat . map Product $ lst
+
+-- instance (Monoid a, Monoid b) => Monoid (a, b) where
+--   mempty = (mempty, mempty)
+--   (a, b) `mappend` (c, d) = (a `mappend` c, b `mappend` d)
