7d43835d677bfa120718834ad69bccf53a292e9c
Kacper Krycki <kacper.krycki@gmail.com>
2017-05-01 13:28:48 +0200

    treeFold done

--- lesson08/Party.hs
+++ lesson08/Party.hs
@@ -1,18 +1,16 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Party
-  where
+module Party where
 
-import Employee
 import Data.Tree
-import Debug.Trace
+import Employee
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e (GL l f) = GL newList updatedFun
   where
-    fun        = empFun e
-    newList    = e:l
+    fun = empFun e
+    newList = e : l
     updatedFun = f + fun
 
 instance Monoid GuestList where
@@ -25,23 +23,33 @@ listFun (GL _ f) = f
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun l1 l2
   | listFun l1 > listFun l2 = l1
-  | otherwise               = l2
+  | otherwise = l2
 
-treeFold :: (Show a, Show b) => (b -> Tree a -> b) -> b -> Tree a -> b
-treeFold f acc t@(Node _ []) = trace ("returning result " ++ show acc) (acc)
-treeFold f acc (Node e (t:ts)) = trace ("current employee " ++ (show e)) (treeFold f acc (Node (rootLabel t) ts))
--- treeFold f acc t = trace ("calling foldl on with acc " ++ (show acc) ++ "and subForest " ++ (show $ subForest t) ++ "\n") (foldl f (acc) (subForest t))
+treeFold
+  :: (Show a, Show b)
+  => (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node x []) = f x []
+treeFold f (Node x ts) = f x (map (\t -> treeFold f t) ts)
+
+sumFun :: Tree Employee -> Fun
+sumFun = treeFold (\e rs -> foldl (+) 0 ((empFun e) : rs))
 
 funniestEmployee :: Tree Employee -> Employee
-funniestEmployee tree = treeFold (\e t -> funnierEmployee e $ rootLabel t) (rootLabel tree) tree
+funniestEmployee = treeFold (\e rs -> foldl (funnierEmployee) e (e : rs))
+
+saddestEmployee :: Tree Employee -> Employee
+saddestEmployee = treeFold (\e rs -> foldl (sadderEmployee) e (e : rs))
+
+oneOfEmployees :: (Employee -> Employee -> Bool) -> Employee -> Employee -> Employee
+oneOfEmployees f e1 e2
+  | f e1 e2 = e1
+  | otherwise = e2
 
 funnierEmployee :: Employee -> Employee -> Employee
-funnierEmployee e1 e2
-  | f1 >= f2  = e1
-  | otherwise = e2
-  where
-    f1 = empFun e1
-    f2 = empFun e2
+funnierEmployee e1 e2 = oneOfEmployees (\x1 x2 -> empFun x1 >= empFun x2) e1 e2
 
-treeSumFun :: Tree Employee -> Fun
-treeSumFun = treeFold (\s t -> s + empFun (rootLabel t)) 0
+sadderEmployee :: Employee -> Employee -> Employee
+sadderEmployee e1 e2 = oneOfEmployees (\x1 x2 -> empFun x1 <= empFun x2) e1 e2
+
+combineGLs :: Employee -> [GuestList] -> GuestList
+combineGLs (Node e ts) =
